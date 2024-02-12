57a97b0e25c6e527972d02bc1d02650ca59d9b74
deltrexgg <deltrexgg@gmail.com>
2024-02-12 20:48:06 +0530

    registeration with dir and upload user info on DB 	modified:   server/package-lock.json 	modified:   server/package.json 	modified:   server/routes/regiseter.js

--- server/routes/regiseter.js
+++ server/routes/regiseter.js
@@ -1,17 +1,32 @@
 const User = require('../models/user');
+const fs = require('fs')
 
 const Register = async(username,password,email)=>{
     try{
+
         const databaseUsername = await User.findOne({user : username},{'_id' : 0, 'user' : 1})
         const databaseEmail = await User.findOne({email : email},{'_id' : 0, 'email' : 1})
 
-        if(!databaseUsername && !databaseEmail)
-        console.log("Registeration Done")
+        if(!databaseUsername && !databaseEmail){
+            const newuser = new User({
+                user : username,
+                password : password,
+                email : email
+            });
+
+            newuser.save()
+            .then(()=>{
+                fs.mkdir('./UserData/'+username, err => {
+                    if (err) {throw err}
+                console.log('Directory created and user data uploaded') })
+            })
+            .catch((err)=>{console.log(err)})
+        }
         else if(!databaseEmail)
         console.log("Username exists")
         else
         console.log("Email exists")
-    }catch(err){console.log(err)}
+    }catch(err){console.log("Register function err -> "+err)}
 }
 
 module.exports= Register;
