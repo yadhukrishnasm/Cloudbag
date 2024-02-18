accd9e306406457b63ea2af8ceba6a02e7497497
yadhukrishnasm <yadhukrishna.me@gmail.com>
2024-02-18 12:11:33 +0530

    home page updates

--- client/src/components/searchbox.js
+++ client/src/components/searchbox.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import './searchbox.css'
 
 export default function searchbox() {
   return (
-    <div>searchbox</div>
+    <div classname = "searchbox">
+      <input type="text" name="search" id="searchBox" />
+      <button type="submit">search</button>
+    </div>
   )
 }
--- client/src/components/textbox.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function textbox() {
-  return (
-    <div>textbox</div>
-  )
-}
