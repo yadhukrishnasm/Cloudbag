a6c6e0509a2d4cc9ddef012cff6171ee7a82b209
yadhukrishnasm <yadhukrishna.me@gmail.com>
2024-02-24 09:49:43 +0530

    updated navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import {Link, useLocation} from 'react-router-dom';
 import Avatar from './avatar';
 import './navbar.css';
 
 export const Navbar = ({username})=>{
-  ;
+  const [shouldDisplay, setShouldDisplay] = useState(true);
+  const location = useLocation();
+
+  useEffect(() => {
+    const currentEndpoint = location.pathname;
+
+    if (currentEndpoint === '/' || currentEndpoint === '/register') {
+      setShouldDisplay(false);
+    } else {
+      setShouldDisplay(true);
+    }
+  }, [location.pathname]);
   return (
     <div>
       <div className="nav">
         <div id='left'>
           <Link to ="/home" id="logo">Cloudbag</Link>
         </div>
-        <div id="right">
-            <span id='links'>
-              <Link className ='link' to ="/ar">AR</Link>
-              <Link className ='link' to ="/gramma">Cloudspell</Link>
-            </span>
-            <span id='user'>
-              <span id='username'>{username}</span>
-              <Avatar/>  
-            </span>
-        </div>
+
+        {shouldDisplay && (
+          <div id="right">
+                <span id='links'>
+                  <Link className ='link' to ="/ar">AR</Link>
+                  <Link className ='link' to ="/gramma">Cloudspell</Link>
+                </span>
+                <span id='user'>
+                  <span id='username'>{username}</span>
+                  <Avatar/>  
+                </span>
+          </div>
+          )}
       </div>
     </div>
   )
