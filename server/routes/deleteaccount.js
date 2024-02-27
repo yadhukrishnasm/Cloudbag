65636bec23f0b8fa803eb50572da8491d89c8361
deltrexgg <deltrexgg@gmail.com>
2024-02-27 22:07:01 +0530

    Account delete feature 	modified:   server/routes/deleteaccount.js

--- server/routes/deleteaccount.js
+++ server/routes/deleteaccount.js
@@ -2,7 +2,19 @@ const User = require('../models/user');
 const fs = require('fs')
 
 const DeleteAccount = async(userid) =>{
+try{
+    const deletedUser = await User.findByIdAndDelete(userid);
+    console.log(deletedUser)
+    if (!deletedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    const filePath = './UserData/'+userid; 
+    fs.rmdirSync(filePath, { recursive: true })
+    .then(()=>{return 1}); 
 
+}catch(err){
+
+}
 }
 
 module.exports = DeleteAccount;
