72d303d95a8391e0b1304c847e5c9ffe304d36d7
deltrexgg <deltrexgg@gmail.com>
2024-03-09 20:23:05 +0530

    file share with response 	modified:   server/routes/sharedata.js

--- server/routes/sharedata.js
+++ server/routes/sharedata.js
@@ -1,26 +1,19 @@
 const User = require('../models/user');
-const fs = require('fs')
-
+const fs = require('fs').promises;
 const path = "./UserData/";
 
-const ShareData = async(userid,filename,resUsername) =>{
-try{
-    const resid = await User.findOne({user : resUsername},{'_id' : 1})
+const ShareData = async (userId, filename, resUsername) => {
+    try {
+        const resid = await User.findOne({ user: resUsername }, { '_id': 1 });
 
-    fs.copyFile(path+userid+"/"+filename, path+resid._id+"/"+filename, (err) => {
-        if (err) {
-            console.error('Error sharing file:', err);
-        } else {
-            console.log('File shared successfully.');
-            return "File shared successfully."
-        }
-    });
-
-}catch(err){
-    console.log("Receiver ID Error"+err)
-    return "Error"
-}
-
+        await fs.copyFile(path + userId + "/" + filename, path + resid._id + "/" + filename);
+        
+        console.log('File shared successfully.');
+        return "File shared successfully.";
+    } catch (err) {
+        console.error('Error sharing file:', err);
+        return "Error";
+    }
 }
 
 module.exports = ShareData;
