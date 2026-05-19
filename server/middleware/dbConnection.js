37066a4eec348066a5bbc461cd1dca65257cd1e3
SM <119155365+yadhukrishnasm@users.noreply.github.com>
2026-05-19 18:08:55 +0530

    Update dbConnection.js

--- server/middleware/dbConnection.js
+++ server/middleware/dbConnection.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const dbConnectionMiddleware = async (req, res) => {
     try {
-        await mongoose.connect('mongodb+srv://admin:root@yadhukrishnasm.hklj5a3.mongodb.net/Cloudbag');
+        await mongoose.connect();
         console.log('Connected to MongoDB');
       } catch (error) {
         console.error('Error connecting to MongoDB:', error);
